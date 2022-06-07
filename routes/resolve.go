4d182c9fb2839a86ae32e886499462047c53bdd0
akshanshgusain <akshanshgusain@gmail.com>
2022-06-07 09:49:59 +0530

    feat: added database and resolver

--- routes/resolve.go
+++ routes/resolve.go
@@ -1,1 +1,31 @@
 package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"urlshortner/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short url not found in the database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Internal Server Error",
+		})
+	}
+	rInc := database.CreateClient(1)
+	defer rInc.Close()
+
+	_ = rInc.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
--- routes/shorten.go
+++ routes/shorten.go
@@ -1,8 +1,13 @@
 package routes
 
 import (
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"os"
+	"strconv"
 	"time"
+	"urlshortner/database"
 	"urlshortner/helpers"
 )
 
@@ -30,6 +35,23 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// Rate limiting
 
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
 	// check if the input is a valid url
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -44,4 +66,7 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 	// enforce TLS
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
+	return nil
 }
